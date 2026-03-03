ce340b9c008c7650a97c6467d85796f8b35b52c9
savioshaju <savioshajum@gmail.com>
2026-03-03 20:16:54 +0530

    Implement frame resizing for display, add project dependencies, and include a new test video.

--- test.py
+++ test.py
@@ -5,6 +5,9 @@ model = YOLO("runs/detect/train/weights/best.pt")
 
 cap = cv2.VideoCapture(0)
 
+def resize(frame):
+    return cv2.resize(frame, (500, 640))    
+
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -14,7 +17,9 @@ while True:
 
     annotated_frame = results[0].plot()
 
-    cv2.imshow("Helmet Detection - Live", annotated_frame)
+    annotated_frame = resize(annotated_frame)
+
+    cv2.imshow("Helmet Detection", annotated_frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
